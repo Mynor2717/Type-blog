96e0eb28077deab0c3b2926223d38c7f5c93a11b
node <ratafak15@gmail.com>
2024-03-11 12:45:15 -0400

    Errores

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
+
 const { withContentlayer } = require("next-contentlayer");
-
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = { reactStrictMode: true, swcMinify: true }
+ 
 module.exports = withContentlayer(nextConfig);
